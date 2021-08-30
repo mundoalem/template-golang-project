f25fa6e20b24886cf537449128b2b4e3a27ba87a
Egon Braun <egonbraun@gmail.com>
2021-08-30 22:49:24 +0200

    Add license header

--- internal/command/version_command.go
+++ internal/command/version_command.go
@@ -1,3 +1,18 @@
+// This file is part of template-golang-project.
+//
+// template-golang-project is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// template-golang-project is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with template-golang-project. If not, see <https://www.gnu.org/licenses/>.
+
 // Package command provides all the commands used by the main function
 package command
 
--- magefile.go
+++ magefile.go
@@ -1,5 +1,20 @@
 // +build mage
 
+// This file is part of template-golang-project.
+//
+// template-golang-project is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// template-golang-project is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with template-golang-project. If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 import (
